d97091b0e5aa8cd2733b0f4bab36cdf6ab28368c
Keith <altovskiy2006@gmail.com>
2026-06-26 23:32:21 +0200

    feat: implement WeatherStatistics model, WeatherAnalyzer class and interface

---
+++ Models/WeatherStatistics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAnalyzer.Models;
+
+public class WeatherStatistics
+{
+    public double AverageTemperature { get; init; }
+
+    public double MinimumTemperature { get; init; }
+
+    public double MaximumTemperature { get; init; }
+
+    public double AverageHumidity { get; init; }
+
+    public double AveragePressure { get; init; }
+
+    public double AverageWindSpeed { get; init; }
+
+    public int RecordsCount { get; init; }
+}
---
+++ Services/WeatherAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using WeatherAnalyzer.Models;
+using WeatherAnalyzer.Services.Interfaces;
+
+namespace WeatherAnalyzer.Services;
+
+public class WeatherAnalyzer : IWeatherAnalyzer
+{
+    public WeatherStatistics Analyze(IEnumerable<WeatherData> weatherData)
+    {
+        var data = weatherData.ToList();
+
+        if (data.Count == 0)
+        {
+            throw new ArgumentException(
+                "Weather data collection cannot be empty.",
+                nameof(weatherData));
+        }
+
+        return new WeatherStatistics
+        {
+            AverageTemperature = Round(data.Average(x => x.Temperature)),
+            MinimumTemperature = data.Min(x => x.Temperature),
+            MaximumTemperature = data.Max(x => x.Temperature),
+
+            AverageHumidity = Round(data.Average(x => x.Humidity)),
+            AveragePressure = Round(data.Average(x => x.Pressure)),
+            AverageWindSpeed = Round(data.Average(x => x.WindSpeed)),
+
+            RecordsCount = data.Count
+        };
+    }
+    private static double Round(double value)
+    {
+        return Math.Round(value, 1);
+    }
+}
