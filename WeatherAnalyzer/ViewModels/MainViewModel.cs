74d17a3ca7d1ff1f7703ee2cef867b46dab87573
Keith <altovskiy2006@gmail.com>
2026-06-27 15:56:48 +0200

    feat: implement SetProperty

---
+++ WeatherAnalyzer/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherAnalyzer.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    private string _city = string.Empty;
+
+    public string City
+    {
+        get => _city;
+        set => SetProperty(ref _city, value);
+    }
+}
