8e2106bd9f40030083dbd2a48e619be8f5f6985c
Keith <altovskiy2006@gmail.com>
2026-06-28 02:11:44 +0200

    feat: implement AnsiTextCleaner

---
+++ WeatherAnalyzer/Helpers/AnsiTextCleaner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+namespace WeatherAnalyzer.Helpers;
+
+public static partial class AnsiTextCleaner
+{
+    public static string Clean(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return string.Empty;
+        }
+
+        return AnsiRegex().Replace(text, string.Empty);
+    }
+
+    [GeneratedRegex(@"\x1B\[[0-9;]*[A-Za-z]")]
+    private static partial Regex AnsiRegex();
+}
